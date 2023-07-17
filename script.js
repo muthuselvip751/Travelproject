233af44191850b8628610b5088b6fe87b63eeef5
muthuselvi <muthuselvip751@gmail.com.com>
2023-07-17 14:39:40 +0530

    img loadmore

--- script.js
+++ script.js
@@ -53,6 +53,19 @@ var swiper = new Swiper(".home-slider", {
 });
 
 // =========================================iimg loadmore=========================================
+let loadMoreBtn = document.querySelector('.package .load-more .btn');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () =>{
+   let boxes = [...document.querySelectorAll('.package .box-container .box')];
+   for (var i = currentItem; i < currentItem + 3; i++){
+      boxes[i].style.display = 'inline-block';
+   };
+   currentItem += 3;
+   if(currentItem >= boxes.length){
+      loadMoreBtn.style.display = 'none';
+   }
+}
 
 // ===========================================review section===================================
 
