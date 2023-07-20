cc3766705658a772e34b6ca7e0b324be7eae588b
muthuselvi <muthuselvip751@gmail.com.com>
2023-07-20 14:09:29 +0530

    display data from the database connection crete

---
+++ display.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Display Data</title>
+    </head>
+<body>
+
+    <h1>Data from the Database</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Address</th>
+                <th>Location</th>
+                <th>Guests</th>
+                <th>Arrivals</th>
+                <th>Leaving</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "mypassword";
+            $dbname = "bookform";
+     $conn = new mysqli($servername, $username, $password, $dbname);
+
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            ?>
+        </tbody>
+    </table>
+
+    
+
+</body>
+</html>
