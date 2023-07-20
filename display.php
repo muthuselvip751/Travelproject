c7805ee724a322d44822102aaa5c04ea4785b0eb
muthuselvi <muthuselvip751@gmail.com.com>
2023-07-20 14:22:17 +0530

    fetch data from database

--- display.php
+++ display.php
@@ -26,6 +26,7 @@
             </tr>
         </thead>
         <tbody>
+            <!-- ===================sql connection -=====================->
             <?php
             $servername = "localhost";
             $username = "root";
@@ -36,11 +37,40 @@
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
-            ?>
-        </tbody>
-    </table>
+//=======================================fetch data================================================
+       
+$sql = "SELECT * FROM tripform";
+$result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
     
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["id"] . "</td>";
+        echo "<td>" . $row["name"] . "</td>";
+        echo "<td>" . $row["email"] . "</td>";
+        echo "<td>" . $row["phone"] . "</td>";
+        echo "<td>" . $row["address"] . "</td>";
+        echo "<td>" . $row["location"] . "</td>";
+        echo "<td>" . $row["guests"] . "</td>";
+        echo "<td>" . $row["arrivals"] . "</td>";
+        echo "<td>" . $row["leaving"] . "</td>";
+        echo "<td>
+                <a href='update.php?id=" . $row["id"] . "'>Update</a>
+                <a href='delete.php?id=" . $row["id"] . "'>Delete</a>
+              </td>";
+        echo "</tr>";
+    }
+} else {
+    echo "<tr><td colspan='10'>No data found</td></tr>";
+}
+
+$conn->close();
+?>
+</tbody>
+</table>
+
+
 
 </body>
 </html>
