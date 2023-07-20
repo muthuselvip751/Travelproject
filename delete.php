5b46d9d88add84bcff000b7caea45080bf2f383f
muthuselvi <muthuselvip751@gmail.com.com>
2023-07-20 17:01:08 +0530

    delete operation create

---
+++ delete.php
@@ -0,0 +1,31 @@
+<?php
+   $servername = "localhost";
+   $username = "root";
+   $password = "mypassword";
+   $dbname = "bookform";
+
+
+
+if(isset($_GET["id"])) {
+    $id=$_GET["id"];
+
+    $conn=new mysqli($servername, $username,$password,$dbname);
+
+    if($conn->connect_error){
+        die("Connection failed: ". $conn->connect_error);
+    }
+    $sql="DELETE FROM tripform WHERE id='$id'";
+    if($conn->query(sql)===TRUE) {
+        header("Location: display.php");
+    }
+    else{
+        echo "error delete data: ". $conn->error;
+    }
+    $conn->close();
+}else{
+    echo "Invalid request.";
+}
+
+
+
+?>
