91c3fb1a5da9b6a712135e2fe43d09cc208bda4c
muthuselvi <muthuselvip751@gmail.com.com>
2023-07-20 14:53:02 +0530

    media queries add

--- display.php
+++ display.php
@@ -30,6 +30,7 @@ table {
     width: 100%;
     border-collapse: collapse;
     margin-top: 20px;
+    /* border:2px solid red; */
 }
 
 th, td {
@@ -51,6 +52,7 @@ td[colspan='8'] {
     padding: 20px;
     font-style: italic;
     color: #888;
+    /* border:2px solid red; */
 }
 td:last-child {
     text-align: center;
@@ -79,8 +81,53 @@ td:last-child a:nth-child(2) {
 td:last-child a:hover {
     background-color: #6c757d;
 }
-        </style>
-    </head>
+
+ /* media queries */
+@media (max-width: 768px) {
+    table {
+        font-size: 14px;
+    }
+
+    th, td {
+        padding: 8px;
+    }
+}
+
+@media (max-width: 576px) {
+    table {
+        font-size: 12px;
+    }
+
+    th, td {
+        padding: 6px;
+    }
+    th {
+        display: none;
+    }
+
+  td:last-child {
+        text-align: center;
+    }
+
+   
+    tbody tr {
+        display: block;
+        margin-bottom: 10px;
+    }
+
+    td {
+        display: block;
+        padding: 6px;
+        /* background-color:gray; */
+    }
+
+    td:last-child {
+        float: right;
+    }
+}
+
+    </style>
+</head>
 <body>
 
     <h1>Data from the Database</h1>
@@ -101,51 +148,52 @@ td:last-child a:hover {
             </tr>
         </thead>
         <tbody>
-            <!-- ===================sql connection -=====================->
             <?php
             $servername = "localhost";
             $username = "root";
             $password = "mypassword";
             $dbname = "bookform";
-     $conn = new mysqli($servername, $username, $password, $dbname);
+
+// ========================== sql connection ======================================
+            $conn = new mysqli($servername, $username, $password, $dbname);
 
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
-//=======================================fetch data================================================
-       
-$sql = "SELECT * FROM tripform";
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $row["id"] . "</td>";
-        echo "<td>" . $row["name"] . "</td>";
-        echo "<td>" . $row["email"] . "</td>";
-        echo "<td>" . $row["phone"] . "</td>";
-        echo "<td>" . $row["address"] . "</td>";
-        echo "<td>" . $row["location"] . "</td>";
-        echo "<td>" . $row["guests"] . "</td>";
-        echo "<td>" . $row["arrivals"] . "</td>";
-        echo "<td>" . $row["leaving"] . "</td>";
-        echo "<td>
-                <a href='update.php?id=" . $row["id"] . "'>Update</a>
-                <a href='delete.php?id=" . $row["id"] . "'>Delete</a>
-              </td>";
-        echo "</tr>";
-    }
-} else {
-    echo "<tr><td colspan='10'>No data found</td></tr>";
-}
+//==================================== Fetch data from the database============================
+            $sql = "SELECT * FROM tripform";
+            $result = $conn->query($sql);
 
-$conn->close();
-?>
-    </tbody>
+            if ($result->num_rows > 0) {
+                
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row["id"] . "</td>";
+                    echo "<td>" . $row["name"] . "</td>";
+                    echo "<td>" . $row["email"] . "</td>";
+                    echo "<td>" . $row["phone"] . "</td>";
+                    echo "<td>" . $row["address"] . "</td>";
+                    echo "<td>" . $row["location"] . "</td>";
+                    echo "<td>" . $row["guests"] . "</td>";
+                    echo "<td>" . $row["arrivals"] . "</td>";
+                    echo "<td>" . $row["leaving"] . "</td>";
+                    echo "<td>
+                            <a href='update.php?id=" . $row["id"] . "'>Update</a>
+                            <a href='delete.php?id=" . $row["id"] . "'>Delete</a>
+                          </td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='10'>No data found</td></tr>";
+            }
+
+            $conn->close();
+            ?>
+        </tbody>
     </table>
 
+    
 
-
-    </body>
-          </html>
+</body>
+</html>
