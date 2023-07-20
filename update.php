658e3285ccea22eb7498d20b2fa2834123feef34
muthuselvi <muthuselvip751@gmail.com.com>
2023-07-20 15:49:46 +0530

    update data show table create

--- update.php
+++ update.php
@@ -58,18 +58,34 @@ else {
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Update Data</title>
-             <head>
-             </head>
+                </head>
 <body>
-<h1>Update data</h1>
-<form method="post">
 
-<input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
-</form>
+    <h1>Update Record</h1>
 
+    <form method="post">
+        <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
+        <label>Name:</label>
+        <input type="text" name="name" value="<?php echo $row["name"]; ?>"><br>
+        <label>Email:</label>
+        <input type="text" name="email" value="<?php echo $row["email"]; ?>"><br>
+        <label>Phone:</label>
+        <input type="text" name="phone" value="<?php echo $row["phone"]; ?>"><br>
+        <label>Address:</label>
+        <input type="text" name="address" value="<?php echo $row["address"]; ?>"><br>
+        <label>Location:</label>
+        <input type="text" name="location" value="<?php echo $row["location"]; ?>"><br>
+        <label>Guests:</label>
+        <input type="text" name="guests" value="<?php echo $row["guests"]; ?>"><br>
+        <label>Arrivals:</label>
+        <input type="text" name="arrivals" value="<?php echo $row["arrivals"]; ?>"><br>
+        <label>Leaving:</label>
+        <input type="text" name="leaving" value="<?php echo $row["leaving"]; ?>"><br>
+        <button type="submit">Update</button>
+    </form>
 
-</body> 
-
+</body>
+</html>
 
 
 
